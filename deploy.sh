94644edaeb02018aa577f5e71de13d2bb45bd4c7
NicKAnderson038 <andersonnick038@gmail.com>
2019-12-06 04:02:43 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,26 @@
 # abort on errors
 set -e
 
-# # build
-# # yarn build
+# build
+echo Building. this may take a minute...
+yarn run build
 
-# # navigate into the build output directory
-# cd dist
+# navigate into the build output directory
+cd dist
 
-# # if you are deploying to a custom domain
-# # echo 'www.example.com' > CNAME
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
-# # git init
-# git add .
-# git commit -m 'deploy'
+# git init
+git add -A
+git commit -m 'deploy'
 
-# # if you are deploying to https://<USERNAME>.github.io
-# # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:NicKAnderson038/DryerAnimation.git master:gh-pages
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:nickanderson038/DryerAnimation.git master:gh-pages
 
-# cd -
+cd -
 
-git subtree push --prefix dist origin gh-pages
+# git subtree push --prefix dist origin gh-pages
