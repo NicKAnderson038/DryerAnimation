2a2eb03fcdb071209d36b8115c70fb9e923f988b
NicKAnderson038 <andersonnick038@gmail.com>
2019-12-06 04:05:05 -0500

    .

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:NicKAnderson038/DryerAnimation.git master:gh-pages
+git push -f git@github.com:nickanderson038/DryerAnimation.git master:gh-pages
 
 cd -
 
